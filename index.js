d21c21d2bf2959d491ff0ac41b81d4ff1951acb2
Jasmin <jasmin.parish@outlook.com>
2022-08-10 16:35:38 -0400

    commit tree d1d42bd87db83fc87e6b2a128b0449095f71123e

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee =  {
+    name:"Jasmin",
+    streetAddress: "5501 Glenridge"
+};
+
+function updateEmployeeWithKeyAndValue() {
+     const newEmployee = {
+         name: "Sam",
+         streetAddress: "11 Broadway"
+     }
+    return newEmployee
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey (employee, key){
+    const newObject = Object.assign({}, employee)
+    delete newObject[key]
+    return newObject;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee;
+}
